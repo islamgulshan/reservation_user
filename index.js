cfce85d3f7ce5dc30250d5c2a512e5e0667a31b2
gulshan <islam.gulshan@gmail.com>
2019-08-22 17:20:52 +0500

    heroku changes

--- index.js
+++ index.js
@@ -4,20 +4,20 @@ var bodyParser = require("body-parser");
 var cors = require("cors");
 var path = require("path");
 
-mongoose.connection.openUri('mongodb://localhost:27017/myfirstproject', { useNewUrlParser:true});
-mongoose.connection.on('connected',()=>{
-	console.log('coonected To db ');
-})
+// mongoose.connection.openUri('mongodb://localhost:27017/myfirstproject', { useNewUrlParser:true});
+// mongoose.connection.on('connected',()=>{
+// 	console.log('coonected To db ');
+// })
 
-mongoose.connection.on('error',(err)=>{
-	if(err){
-		console.log(`error while connection to db ${err}`);
-	}
-})
+// mongoose.connection.on('error',(err)=>{
+// 	if(err){
+// 		console.log(`error while connection to db ${err}`);
+// 	}
+// })
 
 var router =require('./routes/user');
 
-const PORT = 3000;
+const port = 5000;
 
 var app = express();
 app.use(cors());
@@ -33,6 +33,6 @@ app.use(bodyParser.urlencoded({ extended: false }));
 // serving static content
 app.use(express.static(path.join(__dirname, "public")));
 app.use('/api',router);
-app.listen(PORT, () => {
-  console.log(`listening on port ${PORT}`);
+app.listen(process.env.PORT || port , () => {
+  console.log(`listening on port ${port}`);
 });
